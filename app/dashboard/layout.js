0018b0d1711ff24c5da9b59dfb616f3001016ce7
kofiad <kofiadaba@gmail.com>
2024-10-05 22:05:28 +0000

    Refactor DashboardLayout component to add margin to the container and adjust the gap between elements

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,20 +3,20 @@ import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mx-4">
       {/* Header on top */}
       <header className="fixed w-full top-0 z-10 h-20">
         <Header />
       </header>
 
-      <div className="flex flex-grow">
+      <div className="flex flex-grow gap-8">
         {/* Sidebar on the left */}
         <aside className="w-1/4 bg-gray-100 fixed top-20 left-0 h-[calc(100vh-5rem)] z-20">
           <Sidebar />
         </aside>
 
         {/* Main content area next to sidebar */}
-        <main className="ml-[25%] flex-grow p-8 bg-slate-100 overflow-y-auto" style={{ marginTop: '5rem' }}>
+        <main className="ml-[25%] flex-grow bg-slate-100 overflow-y-auto" style={{ marginTop: '5rem' }}>
           {children}
         </main>
       </div>
