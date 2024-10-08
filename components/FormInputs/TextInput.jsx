2ec7d1519efae89acd9d900ef0e96b5d49a30fdf
kofiad <kofiadaba@gmail.com>
2024-10-08 20:15:21 +0000

    Refactor TextInput component to update styles and remove label duplication

--- components/FormInputs/TextInput.jsx
+++ components/FormInputs/TextInput.jsx
@@ -3,12 +3,6 @@
 export default function TextInput({label, name, register, errors, isRequired=true, type="text", className="sm:col-span-2", autoComplete = "off",}) {
   return (
     <div className={className}>
-          <label
-            htmlFor={name}
-            className="block text-sm font-medium leading-6 text-gray-900 mb-2 "
-          >
-            {label}
-          </label>
           <div>
             <input
               {...register(`${name}`, { required: isRequired })}
@@ -16,7 +10,7 @@ export default function TextInput({label, name, register, errors, isRequired=tru
               name={name}
               id={name}
               autoComplete={autoComplete}
-              className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-full border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
               placeholder={`Enter the ${label}`}
             />
             {errors.title && (
