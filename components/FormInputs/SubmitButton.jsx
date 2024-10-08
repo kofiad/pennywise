5d5559c37ab3491b8ec797f8991216e4d00b951f
kofiad <kofiadaba@gmail.com>
2024-10-08 20:44:28 +0000

    Refactor SubmitButton component to update button styles

--- components/FormInputs/SubmitButton.jsx
+++ components/FormInputs/SubmitButton.jsx
@@ -9,7 +9,7 @@ export default function SubmitButton({isLoading, title, }) {
         <button
           disabled
           type="button"
-          className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 inline-flex items-center"
+          className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 inline-flex items-center"
         >
           <svg
             aria-hidden="true"
@@ -33,7 +33,7 @@ export default function SubmitButton({isLoading, title, }) {
       ) : (
         <button
           type="submit"
-          className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-purple-700 rounded-lg focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-900 hover:bg-purple-800"
+          className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-purple-700 rounded-full focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-900 hover:bg-purple-800"
         >
           <Plus className="w-5 h-5 mr-2" />
           <span>Save {title}</span>
