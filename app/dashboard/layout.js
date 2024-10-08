5ef3baa70475ea6f5c895b0777aa115223b11ab6
kofiad <kofiadaba@gmail.com>
2024-10-08 19:23:02 +0000

    Refactor dashboard layout component

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,22 +1,22 @@
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Dashboard/Header";
+import Sidebar from "@/components/Dashboard/Sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen mx-4">
+    <div className="flex flex-col min-h-screen">
       {/* Header on top */}
       <header className="fixed w-full top-0 z-10 h-20">
         <Header />
       </header>
 
-      <div className="flex flex-grow gap-8">
+      <div className="flex flex-grow">
         {/* Sidebar on the left */}
-        <aside className="w-1/4 bg-gray-100 fixed top-20 left-0 h-[calc(100vh-5rem)] z-20">
+        <aside className="w-1/4 fixed top-20 z-20">
           <Sidebar />
         </aside>
 
         {/* Main content area next to sidebar */}
-        <main className="ml-[25%] flex-grow bg-slate-100 overflow-y-auto" style={{ marginTop: '5rem' }}>
+        <main className="ml-[30%] flex-grow bg-purple-100 overflow-y-auto m-8 rounded-lg" style={{ marginTop: '5rem' }}>
           {children}
         </main>
       </div>
