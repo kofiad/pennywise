c5e23cf8743f93450107d1aa12c4af9682a4b5a9
kofiad <kofiadaba@gmail.com>
2024-10-08 20:44:11 +0000

    Refactor NewIncome form layout and styles

--- app/dashboard/income/new/page.jsx
+++ app/dashboard/income/new/page.jsx
@@ -25,13 +25,13 @@ export default function NewIncome() {
         <FormHeader title="New Income" href="/dashboard/income"/>
         {/* Form */}
         <form  className='w-full max-w-4xl mx-auto p-4 my-3 bg-purple-100 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700'>
-          <div className='grid gap-4 sm:grid-cols-2 sm:gap-6'>
+          <div className='w-full grid gap-4 grid-cols-1 sm:gap-6'>
             <TextInput label='Date' name="date" register={register} errors={errors} />
             <TextInput label='Amount' name="amount" register={register} errors={errors} />
             <TextInput label='Source' name="source" register={register} errors={errors} />
             <TextInput label='Category' name="category" register={register} errors={errors} />
             <TextAreaInput label='Description' name="description" register={register} errors={errors} />
-            <SubmitButton isLoading={loading} title="Income"/>
+            <div className="flex items-center justify-center w-full"><SubmitButton isLoading={loading} title="Income"/></div>
           </div>
         </form>
     </div>
