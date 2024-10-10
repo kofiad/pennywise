4e9e461721f202612af4293722c1f6d86b094e08
kofiad <kofiadaba@gmail.com>
2024-10-10 23:07:27 +0000

    Refactor savings update page to fetch data and pass it to NewSavings component

---
+++ app/dashboard/savings/update/[id]/page.jsx
@@ -0,0 +1,10 @@
+import {getData} from "@/lib/getData"
+import NewSavings from '../../new/page';
+
+export default async function Update({params:{id}}) {
+  const data = await getData(`/api/savings/${id}`);
+  console.log(data)
+  return (
+      <NewSavings initialData={data} isUpdate={true}/>
+  )
+}
