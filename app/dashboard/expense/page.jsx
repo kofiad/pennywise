b59f2988673fb42950fc8c8a254510b9001057b5
kofiad <kofiadaba@gmail.com>
2024-10-10 23:15:40 +0000

    Refactor expense page to use DataTable component and display expense data

--- app/dashboard/expense/page.jsx
+++ app/dashboard/expense/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import DataTable from '@/components/Dashboard/DataTable'
+import FormHeader from '@/components/FormInputs/FormHeader'
+import { getData } from '@/lib/getData'
 
-export default function Expense() {
+export default async function Expense() {
+  const expenseData = getData("api/expense")
+  const [expense] = await Promise.all([expenseData])
+
+  // Ensure expense is an array
+  const validExpense = Array.isArray(expense) ? expense : []
+
+  const data = validExpense.map((item) => {
+    return {
+      id: item.id,
+      amount: item.amount,
+      source: item.source,
+      category: item.category,
+      description: item.description,
+      created: item.createdAt,
+      updated: item.updatedAt,
+    }
+  })
+
+  const columns = ["amount", "source", "created", "updated"]
+
   return (
-    <div>Expense</div>
+    <div>
+        {/* Header */}
+        <FormHeader title="Expense" href="/dashboard/home"/>
+        {/* Table */}
+        <div className="rounded-lg p-8">
+           <DataTable data={data} columns={columns} resource="expense"/>
+        </div>
+    </div>
   )
 }
