dfd458a7632c907705e06af32c4b8e0160bdd9ec
kofiad <kofiadaba@gmail.com>
2024-10-10 23:07:17 +0000

    Refactor savings page to use DataTable component

--- app/dashboard/savings/page.jsx
+++ app/dashboard/savings/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import DataTable from '@/components/Dashboard/DataTable'
+import FormHeader from '@/components/FormInputs/FormHeader'
+import { getData } from '@/lib/getData'
 
-export default function Savings() {
+export default async function savings() {
+  const savingsData = getData("api/savings")
+  const [savings] = await Promise.all([savingsData])
+
+  // Ensure savings is an array
+  const validSavings = Array.isArray(savings) ? savings : []
+
+  const data = validSavings.map((item) => {
+    return {
+      id: item.id,
+      amount: item.amount,
+      source: item.source,
+      category: item.category,
+      description: item.description,
+      created: item.createdAt,
+      updated: item.updatedAt,
+    }
+  })
+
+  const columns = ["amount", "source", "created", "updated"]
+
   return (
-    <div>Savings</div>
+    <div>
+        {/* Header */}
+        <FormHeader title="Savings" href="/dashboard/home"/>
+        {/* Table */}
+        <div className="rounded-lg p-8">
+           <DataTable data={data} columns={columns} resource="savings"/>
+        </div>
+    </div>
   )
 }
