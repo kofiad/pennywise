6da8fdf81114cf500435ad1feae3cd83f15ae5cf
kofiad <kofiadaba@gmail.com>
2024-10-10 16:10:38 +0000

    Refactor Sidebar component to add "New" button for non-home links

--- components/Dashboard/Sidebar.jsx
+++ components/Dashboard/Sidebar.jsx
@@ -1,10 +1,10 @@
 "use client";
-import { ClipboardPenLine, HandCoins, Home, PiggyBank, ShoppingCart } from 'lucide-react';
+import { ClipboardPenLine, HandCoins, Home, PiggyBank, Plus, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Sidebar() {
-  const pathname = usePathname(); // Get the current pathname
+  const pathname = usePathname();
   const sidebarLinks = [
     {
       title: "Home",
@@ -34,24 +34,34 @@ export default function Sidebar() {
   ];
 
   return (
-    <div className="flex flex-col p-4 bg-purple-100 rounded-lg ml-4 mt-20 overflow-auto">
-      <div className="flex-grow flex flex-col">
+    <div className="flex flex-col p-4 bg-purple-100 rounded-lg m-4 overflow-auto w-full">
+      <div className="flex-grow flex flex-col w-full">
         {sidebarLinks.map((link, index) => {
           const { title, href, icon: Icon } = link;
           // Check if the current pathname matches the href to highlight the active link
           const isActive = pathname === href;
 
           return (
-            <Link
-              key={index}
-              href={href}
-              className={`flex items-center justify-center gap-4 p-4 rounded-full mb-4 ${
-                isActive ? 'bg-purple-700 text-white' : 'bg-purple-200'
-              }`}
-            >
-              <Icon className="w-6 h-6" />
-              {title}
-            </Link>
+            <div key={index} className="flex items-center justify-between gap-4 p-4 rounded-full mb-4 w-full">
+              <Link
+                href={href}
+                className={`flex items-center gap-4 ${
+                  isActive ? 'bg-purple-700 text-white' : 'bg-purple-200'
+                } w-full p-4 rounded-full`}
+              >
+                <Icon className="w-6 h-6" />
+                {title}
+              </Link>
+              {title !== "Home" && (
+                <button
+                  onClick={() => window.location.href = `${href}/new`}
+                  className="flex items-center justify-center w-10 h-10 bg-purple-500 rounded-full text-white hover:bg-purple-700"
+                >
+                  <Plus className="w-4 h-4" />
+                </button>
+              )}
+
+            </div>
           );
         })}
       </div>
