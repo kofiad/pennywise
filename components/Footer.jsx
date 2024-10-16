f609f537ded8cedd953f51d32b5a77ddfa46cd40
kofiad <kofiadaba@gmail.com>
2024-10-16 18:33:31 +0000

    Add Footer component with purple background

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className="p-4 bg-purple-900 md:p-8 lg:p-10 dark:bg-gray-800">Footer</div>
+    </footer>
+  )
+}
