66647d3168b49069314cf843b9795c9d55008a8f
kofiad <kofiadaba@gmail.com>
2024-10-16 18:32:13 +0000

    Refactor layout.js to add AuthProvider component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "../styles/main.scss";
+import AuthProvider from "@/context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthProvider>
           {children}
+        </AuthProvider>
       </body>
     </html>
   );
