b25deaab581d8d2a2f7c41fa68c436dfd4377aae
kofiad <kofiadaba@gmail.com>
2024-10-17 11:46:37 +0000

    Refactor Footer component to center align content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        <div className="p-4 bg-purple-900 md:p-8 lg:p-10 dark:bg-gray-800">Footer</div>
+        <div className=" flex items-center justify-center p-4 bg-purple-900 md:p-8 lg:p-10 dark:bg-gray-800">
+          <span>Footer</span>
+        </div>
     </footer>
   )
 }
