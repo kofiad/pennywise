df62c862c0a4b7b63d36f7a4234edc5d455d9445
kofiad <kofiadaba@gmail.com>
2024-10-22 06:36:13 +0000

    Refactor Update component to remove console.log statement

--- app/dashboard/savings/update/[id]/page.jsx
+++ app/dashboard/savings/update/[id]/page.jsx
@@ -3,7 +3,7 @@ import NewSavings from '../../new/page';
 
 export default async function Update({params:{id}}) {
   const data = await getData(`/api/savings/${id}`);
-  console.log(data)
+  // console.log(data)
   return (
       <NewSavings initialData={data} isUpdate={true}/>
   )
