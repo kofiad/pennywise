cdaa1a6fb21acdc32338f78fff204eba06bb96b3
kofiad <kofiadaba@gmail.com>
2024-10-22 06:35:47 +0000

    Refactor Update component to remove console.log statement

--- app/dashboard/expense/update/[id]/page.jsx
+++ app/dashboard/expense/update/[id]/page.jsx
@@ -3,7 +3,7 @@ import NewExpense from '../../new/page';
 
 export default async function Update({params:{id}}) {
   const data = await getData(`/api/expense/${id}`);
-  console.log(data)
+  // console.log(data)
   return (
       <NewExpense initialData={data} isUpdate={true}/>
   )
