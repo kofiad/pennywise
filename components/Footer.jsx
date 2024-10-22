50c41ac8a9c0a15f8fc0cd36d302122ce94962c4
kofiad <kofiadaba@gmail.com>
2024-10-22 20:30:49 +0000

    Refactor Footer component to improve layout and remove unused social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,13 @@
-import React from 'react';
-
 export default function Footer() {
   return (
     <footer className="bg-purple-900 dark:bg-gray-800 p-4 md:p-8 lg:p-10">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="text-white mb-4 md:mb-0">
+      <div className="w-full max-w-6xl mx-auto flex flex-col text-center space-y-4">
+        <div className="text-white">
           <span>&copy; 2024 PennyWise. All rights reserved.</span>
         </div>
-        <div className="flex space-x-4 mb-4 md:mb-0">
+        <div className="flex items-center justify-center space-x-4">
           <a href="/about" className="text-white hover:text-gray-400">About</a>
           <a href="/contact" className="text-white hover:text-gray-400">Contact</a>
-        </div>
-        <div className="flex space-x-4">
-          <a href="https://facebook.com" className="text-white hover:text-gray-400">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="https://twitter.com" className="text-white hover:text-gray-400">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://instagram.com" className="text-white hover:text-gray-400">
-            <i className="fab fa-instagram"></i>
-          </a>
         </div>
       </div>
     </footer>
