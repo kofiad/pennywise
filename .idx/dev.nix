0d50871c09334370f7f2dcedeae342cae14586ed
qwertyriks <romeokumatse@gmail.com>
2024-10-22 21:51:47 +0000

    workspace is started or restarted

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 {pkgs}: {
   # Which nixpkgs channel to use.
-  # Specify the nixpkgs channel version (stable or unstable)
+  # Specify the nixpkgs channel version (stable or unstable).
   channel = "stable-24.05"; # or "unstable"
 
   # List of packages to be included in the environment
