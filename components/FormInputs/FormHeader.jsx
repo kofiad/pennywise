a9e60e7d44fb15d317c80be696b67dc7f9e6d3a0
kofiad <kofiadaba@gmail.com>
2024-10-22 14:10:46 +0000

    Refactor FormHeader component to fix margin typo

--- components/FormInputs/FormHeader.jsx
+++ components/FormInputs/FormHeader.jsx
@@ -3,7 +3,7 @@ import { X } from 'lucide-react'
 
 export default function FormHeader({title, href}) {
   return (
-    <div className="mt-5 rounded-lg flex items-center justify-between bg-white py-3 px-8">
+    <div className="m-5 rounded-lg flex items-center justify-between bg-white py-3 px-8">
           <h2 className='text-xl font-semibold'>{title}</h2>
           <a href={href}>
             <X className="h-6 w-6" />
