c77619b18c4c0e74823295adbf6c21dbc928cdb5
qwertyriks <romeokumatse@gmail.com>
2024-10-22 21:46:02 +0000

    workspace is started or restarted

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -16,12 +16,12 @@
     pkgs.nodePackages.pnpm
     pkgs.bun
   ];
-  # Sets environment variables in the workspace.
-  # Define environment variables (currently empty, can be customized).
+  # Sets environment variables in the workspace
+  # Define environment variables (currently empty, can be customized)
   
   idx = {
-    # Extensions to be added to the environment.
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id".
+    # Extensions to be added to the environment
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
@@ -34,7 +34,7 @@
         # Install dependencies without audit, optimizing for offline use
         npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
 
-        # List of files to open by default, if they exist in the workspacey
+        # List of files to open by default, if they exist in the workspace
         # Open editors for the following files by default, if they exist:
         default.openFiles = [
           # Cover all the variations of language, src-dir, router (app/pages)
